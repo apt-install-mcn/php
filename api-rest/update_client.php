cbd6ec4e4fffc33c0692d72c3f76e7e1953d2f21
apt-install-mcn <moosescn20@gmail.com>
2025-01-29 14:15:19 -0500

    update

--- api-rest/update_client.php
+++ api-rest/update_client.php
@@ -1,5 +1,5 @@
 <?php
-include_once 'includes/Database.class.php';
+include_once '../includes/Database.class.php';
 include_once 'auth.php';
 
 authenticate();
@@ -9,13 +9,31 @@ $db = $database->getConnection();
 
 $data = json_decode(file_get_contents("php://input"));
 
-if (!empty($data->id) && (!empty($data->nombre) || !empty($data->email))) {
-    $query = "UPDATE clientes SET nombre = :nombre, email = :email WHERE id = :id";
+if (!empty($data->id)) {
+    $query = "UPDATE clientes SET ";
+    $params = [];
+
+    if (!empty($data->nombre)) {
+        $query .= "nombre = :nombre ";
+        $params[':nombre'] = $data->nombre;
+    }
+
+    if (!empty($data->email)) {
+        if (!empty($data->nombre)) {
+            $query .= ", ";
+        }
+        $query .= "email = :email ";
+        $params[':email'] = $data->email;
+    }
+
+    $query .= "WHERE id = :id";
+    $params[':id'] = $data->id;
+
     $stmt = $db->prepare($query);
 
-    $stmt->bindParam(':nombre', $data->nombre);
-    $stmt->bindParam(':email', $data->email);
-    $stmt->bindParam(':id', $data->id);
+    foreach ($params as $key => &$val) {
+        $stmt->bindParam($key, $val);
+    }
 
     if ($stmt->execute()) {
         echo json_encode(array("message" => "Cliente actualizado."));
